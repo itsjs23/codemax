8e1e24fd100e6795996a828e807660cfe5e2db11
jhamman sharma <itsjs23@gmail.com>
2018-07-19 10:03:51 +0530

    Added ReadMe Files

--- app/admin/controllers/Create-quiz/Create_quiz.php
+++ app/admin/controllers/Create-quiz/Create_quiz.php
@@ -17,38 +17,17 @@ class Create_quiz extends Controller {
             $new_exam_id = $model->createNewExam();
             Session::put('quizId', $new_exam_id);
         }
-        Link::sendTo('manage-exam/id/' . $new_exam_id);
+        Link::sendTo('manage-quiz/id/' . $new_exam_id);
     }
 
 
     public function old_exam($params) {
         Session::put('quizId', $params[0]);
-        Link::sendTo('manage-exam/id/' . $params[0]);
+        Link::sendTo('manage-quiz/id/' . $params[0]);
     }
 
     public function delete_exam($params) {
         $model = new Create_exam_model;
         $model->deleteExam((int) $params[0]);
     }
-
-    public function class_wise_exams_count() {
-        $postData = json_decode($_POST['data'], true);
-        $this->data['classes'] = (new Create_exam_model)->getClassWiseExamsCount($postData['medium'], $postData['exam_type']);
-        $this->data['exam_type'] = $postData['exam_type'];
-        $this->data['medium'] = $postData['medium'];
-        $this->view = ['pages/ce-class-wise-exams-count-template'];
-        $this->display();
-    }
-
-    public function class_wise_exams_list() {
-        $postData = json_decode($_POST['data'], true);
-        $this->data['sections'] = (new Create_exam_model)->getSectionWiseExamCount($postData['medium'], $postData['standard'], $postData['exam_type']);
-        $this->data['exams'] = (new Create_exam_model)->getClassWiseExamsList($postData['medium'], $postData['standard'], $postData['exam_type']);
-        $this->data['exam_type'] = $postData['exam_type'] === 'uploaded' ? 'Question Paper' : $postData['exam_type'];
-        $this->data['medium'] = $postData['medium'];
-        $this->data['standard'] = $postData['standard'];
-        $this->view = ['pages/ce-class-wise-exams-list-template'];
-        $this->display();
-    }
-
 }
---
+++ readme.md
@@ -0,0 +1,12 @@
+-------- CREATE QUIZ ----------
+Username : admin
+password : 123
+
+----- USING URL MODE REWRITE CONDITION  -------
+
+1.   RewriteBase "codemax" In .htaccess
+2.   VIRTUAL_LOCATION = "codemax"
+     BASE_URL ="http://localhost/codemax/"
+
+
+
